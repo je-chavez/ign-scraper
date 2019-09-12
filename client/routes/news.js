f794addb6ee1aec5b4238f67e7d86a1638e6a739
Juan Chavez <juanchavez017@gmail.com>
2019-09-12 11:27:18 -0700

    Added some simple error handling for offset and limit

--- client/routes/news.js
+++ client/routes/news.js
@@ -9,14 +9,49 @@ module.exports = function() {
     let limit = parseInt(req.query.limit);
     let offset = parseInt(req.query.offset);
 
-    if(isNaN(limit)){
-        res.status(400).send({success: false, error: 'Limit is not a number. Please try again.'})
-        return;
+    if (isNaN(limit)) {
+      res.status(400).send({
+        success: false,
+        error: 'Limit is not a number. Please try again.'
+      });
+      return;
+    } else if (limit > 100) {
+      res.status(400).send({
+        success: false,
+        error:
+          'Limit is a value greater than allowed. Please choose a value under 100.'
+      });
+      return;
+    } else if (limit < 0) {
+      res.status(400).send({
+        success: false,
+        error:
+          'Limit is a negative value. Please choose a number greater than 0.'
+      });
+      return;
     }
-    if(isNaN(offset)){
-        res.status(400).send({success: false, error: 'Offset is not a number. Please try again.'})
-        return;
-    }    
+
+    if (isNaN(offset)) {
+      res.status(400).send({
+        success: false,
+        error: 'Offset is not a number. Please try again.'
+      });
+      return;
+    } else if (offset > 100) {
+      res.status(400).send({
+        success: false,
+        error:
+          'Offset is a value greater than allowed. Please choose a value under 100.'
+      });
+      return;
+    } else if (offset < 0) {
+      res.status(400).send({
+        success: false,
+        error:
+          'Offset is a negative value. Please choose a number greater than 0.'
+      });
+      return;
+    }
 
     try {
       let news = await News.find()
@@ -26,12 +61,10 @@ module.exports = function() {
       res.status(200).send({ success: true, news });
     } catch (err) {
       console.log(err);
-      res
-        .status(500)
-        .send({
-          success: false,
-          error: 'Error occurred trying to get records. Try again later.'
-        });
+      res.status(500).send({
+        success: false,
+        error: 'Error occurred trying to get records. Try again later.'
+      });
     }
   });
 
